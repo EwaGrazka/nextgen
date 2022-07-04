53f6647696a0648390a458a6cc23c1a9eaec1ded
EwaGrazka <fk.grazka@gamil.com>
2022-07-04 16:13:56 +0200

    products mobile add

--- src/app/components/SingleProduct/SingleProduct.jsx
+++ src/app/components/SingleProduct/SingleProduct.jsx
@@ -5,13 +5,14 @@ import { Link } from 'react-router-dom';
 export const SingleProduct = ({ isNight, productIMG, price, title, id }) => {
 
     return (
-        <div className={!isNight ? 'product-n' : 'product'}>
+        <div className={isNight ? 'product green green-border' : 'product blue blue-border'}>
 
 
             <Link to={{
                 pathname: `/product${id}`
             }}>  <img className='product-img' src={productIMG} alt='product' />
-                <h8 className='product-title'>{title} <span>{price}</span></h8>
+                <h8 className='product-title green
+                '>{title} <span >{price}</span></h8>
             </Link>
 
         </div>
--- src/app/pages/Products/Products.jsx
+++ src/app/pages/Products/Products.jsx
@@ -4,18 +4,18 @@ import './Products.scss';
 import { useTranslation } from 'react-i18next';
 import { SingleProduct } from '../../components/SingleProduct/SingleProduct'
 import { ProductsBase } from '../../base/ProductsBase.jsx';
-import { useLocation } from 'react-router-dom';
-
 
 export const Products = ({ isNight, title, id }) => {
     const { t, i18n } = useTranslation('common');
-    const location = useLocation();
-    console.log(location.hash)
 
     return (
         <>
-            <h3 id={id} className={!isNight ? 'category-title' : 'category-title-n'} isNight={isNight}>{title}</h3>
-            <div className='products-content'>
+            <h3
+                className={isNight ? 'category-title green' : 'category-title blue'}
+            >
+                {title}
+            </h3>
+            <div className={isNight ? 'products-content green' : 'products-content blue'}>
                 {ProductsBase.map((p) => (
                     <SingleProduct id={p.id} isNight={isNight} productIMG={p.examplePhoto} price={p.price} title={p.title} />
                 ))}
